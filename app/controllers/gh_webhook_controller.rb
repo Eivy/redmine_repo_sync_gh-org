8ae22b7650503e0fcd5812ca3d991dcc623426e6
Eivy <modern.times.rock.and.roll+git@gmail.com>
2021-11-12 18:06:08 +0900

    Sync repo if push event come

--- app/controllers/gh_webhook_controller.rb
+++ app/controllers/gh_webhook_controller.rb
@@ -4,33 +4,45 @@ class GhWebhookController < ApplicationController
   wrap_parameters format: [:json]
 
   def handler
-    puts request.headers["X-GitHub-Event"] 
-    if request.headers["X-GitHub-Event"] != "repository" then
-      return
+    puts "new webhook events #{request.headers['X-GitHub-Event']}"
+    if request.headers["X-GitHub-Event"] == "repository" then
+      data = params['gh_webhook']
+      if data['action'] != "created" then
+        puts 'not created event'
+        return
+      end
+      setting = Setting.plugin_redmine_repo_sync_gh_org
+      dir = setting['repository_root'] 
+      if dir == '' then
+        puts 'not set valid conhfig'
+        return
+      end
+      FileUtils.mkdir_p(dir) unless File.exists?(dir)
+      if data['repository']['private'] and setting['github_user'] == '' then
+        puts 'cloning private repo with no auth info'
+        return
+      end
+      repo = data['repository']['clone_url']
+      if data['repository']['private'] then
+        repo = repo.gsub("://", "://#{CGI.escape(setting["github_user"])}:#{CGI.escape(setting["github_token"])}@")
+      end
+      cmd = "git clone --bare #{repo}"
+      puts cmd
+      spawn(cmd, chdir: dir)
     end
-    data = params['gh_webhook']
-    if data['action'] != "created" then
-      puts 'not created event'
-      return
+    if request.headers["X-GitHub-Event"] == "push" then
+      setting = Setting.plugin_redmine_repo_sync_gh_org
+      data = params['gh_webhook']
+      name = File.join(setting['repository_root'], data['repository']['clone_url'].split('/').last)
+      projects = Project.active.has_module(:repository)
+      projects.each do |project|
+        project.repositories.each do |repo|
+          if repo.url == name then
+            repo.fetch_changesets
+          end
+        end
+      end
     end
-    setting = Setting.plugin_redmine_repo_sync_gh_org
-    dir = setting['repository_root'] 
-    if dir == '' then
-      puts 'not set valid conhfig'
-      return
-    end
-    FileUtils.mkdir_p(dir) unless File.exists?(dir)
-    if data['repository']['private'] and setting['github_user'] == '' then
-      puts 'cloning private repo with no auth info'
-      return
-    end
-    repo = data['repository']['clone_url']
-    if data['repository']['private'] then
-      repo = repo.gsub("://", "://#{CGI.escape(setting["github_user"])}:#{CGI.escape(setting["github_token"])}@")
-    end
-    cmd = "git clone --bare #{repo}"
-    puts cmd
-    spawn(cmd, chdir: dir)
   end
 
 end
