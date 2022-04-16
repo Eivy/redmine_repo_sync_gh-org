1a3b8ef4862fa9ddf11c04a25d7d1b696a8a0c80
Eivy <modern.times.rock.and.roll+git@gmail.com>
2022-04-16 16:57:53 +0900

    bump up version to 0.0.2

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_repo_sync_gh_org do
   name 'Redmine Repo Sync Gh Org plugin'
   author 'Eivy'
   description 'This is a plugin for Redmine'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://github.com/Eivy/redmine_repo_sync_gh_org'
   author_url 'https://eivy.github.io/'
   settings :partial => 'settings/gh_webhook_settings', :default => {'repository_root' => '', 'github_user' => '', 'github_token' => ''}
